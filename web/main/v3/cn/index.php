915deb33306355200645d84413a6fa627bfd0c42
Allen Galler <allengaller@gmail.com>
2014-12-11 21:16:47 +0800

    update
    update

--- web/main/v3/cn/index.php
+++ web/main/v3/cn/index.php
@@ -9,23 +9,15 @@
           <p class="pull-right visible-xs">
             <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
           </p>
-          <div class="jumbotron" style="min-height: 550px;">
+          <div class="jumbotron" style="min-height: 350px;">
             <div style="float:left; min-width: 500px">
              <b>Allen Galler</b><br/>
              <b>Full-stack Web Developer</b><br/><br/>
              <i class="fa fa-flag"></i>&nbsp;Focus on SaaS Architecture Design and Coding.<br/>
              <i class="fa fa-heart"></i>&nbsp;Interested in PaaS and Wearable.<br/>
              <i class="fa fa-leaf"></i>&nbsp;Loving publishing, aerobic exercise, piano and yoga.<br/>
-            </div>
-            <div>
-              <b>亚仑</b><br/>
-              <b>全栈 Web 开发者</b><br/><br/>
-              <i class="fa fa-flag"></i>&nbsp;专注于SaaS系统架构设计与研发.<br/>
-              <i class="fa fa-heart"></i>&nbsp;兴趣在PaaS和智能可穿戴设备.<br/>
-              <i class="fa fa-leaf"></i>&nbsp;业余爱好是技术出版,有氧运动,钢琴和瑜伽.<br/>
-            </div>
-            <br/>
-            <div>
+              
+              <br/>
               <i class="fa fa-plug"></i>&nbsp;<a href="http://blog.allengaller.com/">Blog 博客</a>
               &nbsp;&nbsp;
               <i class="fa fa-lightbulb-o"></i>&nbsp;<a href="studio.php">Studio 工坊</a>
@@ -33,49 +25,40 @@
               <i class="fa fa-globe"></i>&nbsp;<a href="community.php">Community 社区</a>
               &nbsp;&nbsp;
               <i class="fa fa-book"></i>&nbsp;<a href="publishing.php">Publishing 出版</a>
-              &nbsp;&nbsp;
+
             </div>
-            
-            <br/><hr><br/>
-            
             <div>
+              <b>亚仑</b><br/>
+              <b>全栈 Web 开发者</b><br/><br/>
+              <i class="fa fa-flag"></i>&nbsp;专注于SaaS系统架构设计与研发.<br/>
+              <i class="fa fa-heart"></i>&nbsp;兴趣在PaaS和智能可穿戴设备.<br/>
+              <i class="fa fa-leaf"></i>&nbsp;业余爱好是技术出版,有氧运动,钢琴和瑜伽.<br/>
               <i class="fa fa-github-alt"></i>&nbsp;<a href="https://github.com/allengaller">Github</a>
               &nbsp;&nbsp;
-              <i class="fa fa-codepen"></i>&nbsp;<a href="https://github.com/allengaller">Codepen</a>
+               <i class="fa fa-weixin"></i>&nbsp;<a href="https://github.com/allengaller">Weixin</a>:allengaller
               &nbsp;&nbsp;
+              <i class="fa fa-qq"></i>&nbsp;<a href="https://github.com/allengaller">QQ</a>:517946367
+              &nbsp;&nbsp;
+              <i class="fa fa-weibo"></i>&nbsp;Weibo:<a href="https://github.com/allengaller">@allengaller</a>
+              &nbsp;&nbsp;
+
+            </div>
+              
+              <i class="fa fa-send-o"></i>&nbsp;<a href="https://github.com/allengaller">allengaller(at)gmail.com</a>
+              &nbsp;&nbsp;
+              <i class="fa fa-send-o"></i>&nbsp;<a href="https://github.com/allengaller">allengaller(at)qq.com</a>
+              &nbsp;&nbsp;
+
               <i class="fa fa-linkedin"></i>&nbsp;<a href="http://www.linkedin.com/profile/view?id=51164356">LinkedIn</a>
+              &nbsp;&nbsp;
+              <i class="fa fa-google-plus"></i> &nbsp;<a href="https://github.com/allengaller">G+</a>
+              &nbsp;&nbsp;
+              <i class="fa fa-skype"></i>&nbsp;<a href="https://github.com/allengaller">Skype</a>: allengaller
               &nbsp;&nbsp;
               <i class="fa fa-facebook"></i> &nbsp;<a href="https://www.facebook.com/allen.jobs.galler">Facebook</a>
               &nbsp;&nbsp;
               <i class="fa fa-twitter"></i> &nbsp;<a href="https://twitter.com/AllenGaller">Twitter</a>
               &nbsp;&nbsp;
-              <i class="fa fa-google-plus"></i> &nbsp;<a href="https://github.com/allengaller">G+</a>
-              &nbsp;&nbsp;
-              <i class="fa-send-o"></i>&nbsp;<a href="https://github.com/allengaller">allengaller(at)gmail.com</a>
-              &nbsp;&nbsp;
-              <i class="fa-send-o"></i>&nbsp;<a href="https://github.com/allengaller">allengaller(at)qq.com</a>
-              &nbsp;&nbsp;
-              <i class="fa fa-qq"></i>&nbsp;<a href="https://github.com/allengaller">QQ</a>:517946367
-              &nbsp;&nbsp;
-              <i class="fa fa-weixin"></i>&nbsp;<a href="https://github.com/allengaller">Weixin</a>:allengaller
-              &nbsp;&nbsp;
-              <i class="fa fa-weibo"></i>&nbsp;Weibo:<a href="https://github.com/allengaller">@allengaller</a>
-              &nbsp;&nbsp;
-              <i class="fa fa-skype"></i>&nbsp;<a href="https://github.com/allengaller">Skype</a>: allengaller
-              <br/><hr><br/>
-              <b>Sharing</b><br/>
-              <i class="fa fa-steam"></i>&nbsp;<a href="https://github.com/allengaller">Steam</a>
-              &nbsp;
-              <i class="fa fa-tumblr"></i>&nbsp;<a href="https://github.com/allengaller">Tumblr</a>
-              &nbsp;
-              <i class="fa fa-flickr"></i>&nbsp;<a href="https://github.com/allengaller">flickr</a>
-              &nbsp;
-              <i class="fa fa-dribbble"></i>&nbsp;<a href="https://github.com/allengaller">Dribbble</a>
-              &nbsp;
-              <i class="fa fa-slideshare"></i>&nbsp;<a href="https://github.com/allengaller">slideshare</a>
-              &nbsp;
-              <i class="fa fa-youtube-play"></i> &nbsp;<a href="https://github.com/allengaller">youtube</a>
-              &nbsp;
             </div>
           </div>  
         </div>

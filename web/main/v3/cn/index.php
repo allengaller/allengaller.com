c0baf5735e2193c6f0a570cda4c82914e1ff8712
allengaller <allengaller@qq.com>
2014-12-30 10:58:53 +0800

    update
    update

--- web/main/v3/cn/index.php
+++ web/main/v3/cn/index.php
@@ -12,36 +12,22 @@
           <div class="jumbotron" style="min-height: 560px;">
             <div style="float:left; min-width: 500px">
              <b>Allen Galler 亚仑</b><br/>
-             <b>Full-stack Web Developer | 全栈 Web 开发者</b><br/><br/>
-             <i class="fa fa-flag"></i>&nbsp;Focus on SaaS Architecture Design and Coding.<br/>
-             &nbsp;&nbsp;&nbsp;专注于SaaS系统架构设计与研发.<br>
-             <i class="fa fa-heart"></i>&nbsp;Interested in PaaS and Wearable.<br/>
-             &nbsp;&nbsp;&nbsp;兴趣在PaaS和智能可穿戴设备.<br/>
-             <i class="fa fa-leaf"></i>&nbsp;Loving publishing, aerobic exercise, piano and yoga.<br/>
-             &nbsp;&nbsp;&nbsp;业余爱好是技术出版,有氧运动,钢琴和瑜伽.<br/>
-
              <br/><br/>
-
-              <i class="fa fa-plug"></i>&nbsp;<a target="_new" href="http://blog.allengaller.com/">Blog 博客</a>&nbsp;&nbsp;
-              <i class="fa fa-lightbulb-o"></i>&nbsp;<a href="studio.php">Studio 工坊</a>&nbsp;&nbsp;
-
-              <br/><br/>
-              <i class="fa fa-globe"></i>&nbsp;<a href="community.php">Community 社区</a>&nbsp;&nbsp;
-              <i class="fa fa-book"></i>&nbsp;<a href="publishing.php">Publishing 出版</a>
-              
-              <br/><br/>
-
+             <b>Full-stack Web Developer | 全栈 Web 开发者</b><br/>
+             <br/><br/>
+             <i class="fa fa-flag"></i>&nbsp;Focus on SaaS Architecture Design and Coding. | 专注于SaaS系统架构设计与研发.<br/>
+             <br/><br/>
+             <i class="fa fa-heart"></i>&nbsp;Interested in PaaS and Wearable. | 兴趣于PaaS和智能可穿戴设备.<br/>
+             <br/><br/>
+             <i class="fa fa-leaf"></i>&nbsp;Loving publishing, aerobic exercise, piano and yoga. |　业余爱好是技术出版,有氧运动,钢琴和瑜伽.<br/>
+             <br/><br/>
               <i class="fa fa-github-alt"></i>&nbsp;Github: <a href="https://github.com/allengaller">allengaller</a>&nbsp;&nbsp;
               <i class="fa fa-send-o"></i>&nbsp;Mail: allengaller(at)gmail.com</a>&nbsp;&nbsp;
-              
               <br/><br/>
-
               <i class="fa fa-weixin"></i>&nbsp;Weixin: allengaller&nbsp;&nbsp;
               <i class="fa fa-qq"></i>&nbsp;QQ: 517946367&nbsp;&nbsp;
-              <i class="fa fa-weibo"></i>&nbsp;Weibo:<a href="http://weibo.com/517946367">@allengaller</a>&nbsp;&nbsp;
-              
+              <i class="fa fa-weibo"></i>&nbsp;Weibo:<a href="http://weibo.com/517946367">@allengaller</a>&nbsp;&nbsp;  
               <br/><br/>
-
               <i class="fa fa-linkedin"></i>&nbsp;<a href="http://www.linkedin.com/profile/view?id=51164356">LinkedIn</a>&nbsp;&nbsp;
               <i class="fa fa-google-plus"></i> &nbsp;<a href="https://plus.google.com/+AllenGaller/">G+</a>&nbsp;&nbsp;
               <i class="fa fa-facebook"></i> &nbsp;<a href="https://www.facebook.com/allen.jobs.galler">Facebook</a>&nbsp;&nbsp;

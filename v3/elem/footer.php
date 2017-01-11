8f1b70a064a95eadd60092a42e90b52250f181e3
faxi <yalun.cyl@alibaba-inc.com>
2017-01-11 23:38:52 +0800

    update

---
+++ v3/elem/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <p style="text-align:center"><i class="fa fa-copyright"></i> allengaller.com 2009-2014</p>
+	<!-- <p><a href="https://www.upyun.com/"><img src="http://upfiles.b0.upaiyun.com/logo/90x45.png" alt="UPYUN"></a></p> -->
+</footer>
---
+++ v3/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php require('elem/head.php');?>
+  <body>
+    <?php require('elem/nav.php');?>
+    <div class="container">
+      <div class="row row-offcanvas row-offcanvas-right">
+        <div class="col-xs-12 col-sm-12">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <div class="jumbotron" style="min-height: 560px;">
+            <div style="float:left; min-width: 500px">
+             <b>Allen Galler 亚仑</b><br/>
+             <br/><br/>
+             <b>Full-stack Web Developer | 全栈 Web 开发者</b><br/>
+             <br/><br/>
+             <i class="fa fa-flag"></i>&nbsp;Focus on SaaS Architecture Design and Coding. | 专注于SaaS系统架构设计与研发.<br/>
+             <br/><br/>
+             <i class="fa fa-heart"></i>&nbsp;Interested in PaaS and Wearable. | 兴趣于PaaS和智能可穿戴设备.<br/>
+             <br/><br/>
+             <i class="fa fa-leaf"></i>&nbsp;Loving publishing, aerobic exercise, piano and yoga. |　业余爱好是技术出版,有氧运动,钢琴和瑜伽.<br/>
+             <br/><br/>
+              <i class="fa fa-github-alt"></i>&nbsp;Github: <a href="https://github.com/allengaller">allengaller</a>&nbsp;&nbsp;
+              <i class="fa fa-send-o"></i>&nbsp;Mail: allengaller(at)gmail.com</a>&nbsp;&nbsp;
+              <br/><br/>
+              <i class="fa fa-weixin"></i>&nbsp;Weixin: allengaller&nbsp;&nbsp;
+              <i class="fa fa-qq"></i>&nbsp;QQ: 517946367&nbsp;&nbsp;
+              <i class="fa fa-weibo"></i>&nbsp;Weibo:<a href="http://weibo.com/517946367">@allengaller</a>&nbsp;&nbsp;  
+              <br/><br/>
+              <i class="fa fa-linkedin"></i>&nbsp;<a href="http://www.linkedin.com/profile/view?id=51164356">LinkedIn</a>&nbsp;&nbsp;
+              <i class="fa fa-google-plus"></i> &nbsp;<a href="https://plus.google.com/+AllenGaller/">G+</a>&nbsp;&nbsp;
+              <i class="fa fa-facebook"></i> &nbsp;<a href="https://www.facebook.com/allen.jobs.galler">Facebook</a>&nbsp;&nbsp;
+              <i class="fa fa-twitter"></i> &nbsp;<a href="https://twitter.com/AllenGaller">Twitter</a>&nbsp;&nbsp;
+
+            </div>
+            </div>
+          </div>  
+        </div>
+      </div><!--/row-->
+      <hr>
+      <?php require('elem/footer.php');?>
+    </div><!--/.container-->
+    <?php require('elem/js.php');?>
+    <div id="cli_dialog"></div>
+  <script type="text/javascript" src="http://tajs.qq.com/stats?sId=38878023" charset="UTF-8"></script>
+  </body>
+</html>
